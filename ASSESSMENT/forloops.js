eb900f5187b7ae207246d59b1e30efda0e74655d
Tailarry <104940194+Tailarry@users.noreply.github.com>
2022-07-25 15:38:13 -0500

    Add files via upload

---
+++ ASSESSMENT/forloops.js
@@ -0,0 +1,55 @@
+// // /// NOTE initializer: starting value
+// // //condition: "run this code if true" (return true/false)
+// // //iteration: adds on to the intial value to continue the loop
+
+// //NOTE : Description: The "for" loop is the most common loop that used and is typically used 
+// //to run a chunk of code a certain amount of times
+
+// //NOTE syntax
+// for (initializer; condition; iteration){
+//         //chunk of code
+// }
+
+// //NOTE example
+// //initializer:
+// //condition
+// //iteration
+// for (let i = 0; i <= 5; i++){
+//     //chunk of code
+//     console.log(i);
+// };
+
+// let dbz = ["Goku", "Vegeta", "Piccolu", "Kid Buu","Bulma" ];
+
+// for (let j = 0; j < dbz.length; j++){
+
+// console.log(dbz[j])
+
+// };
+
+// // NOTE method one: print ot only the items that start with "p"
+
+// let fruits = ["Apple", "Orange","Tomato","Watermelon","Jackfruit","Tamarindo","Kiwi","Peach"];
+
+// for (let l = 0; l <fruits.length; l++){
+//     if (fruits[1].startsWith("T")) {
+//         console.log(fruits[l]);
+//     }
+//     };
+//     let c10 = ["Mike",""]
+
+//     for (let m = 5; m < 10; m--){
+//         console.log(m)
+//     };
+const ting
+    let  Luggage ('20, 10, 5, 5')
+
+    let = "cargo" ((l = Luggage.length) (l <=0 (l++)));{
+         {
+            
+            console.log(Luggage[l]);
+        }
+        };
+        for (let a = 30; a  <= 50;){
+            console.log(Luggage)
+        };
---
+++ ASSESSMENT/while.js
@@ -0,0 +1,38 @@
+// /// NOTE initializer: starting value
+// //condition: "run this code if true" (return true/false)
+// //iteration: adds on to the intial value to continue the loop
+
+// // NOTE description: The most simple looking statement in javascript! 
+// //loops through a chunk of code as lon as the condition is true
+
+// // Note syntax:
+
+// let i = 0; // initializer //
+
+// while (condition) {
+//     //chunk of code
+//     i++;// iteration post increments
+// } //
+
+// // NOTE example:
+// // TRUE/Success
+
+// let j = 0;
+
+
+// while (j <= 10) {
+//     console.log(j);
+//     j++;
+//     console.log("Current Val:" + j);
+// }
+
+// False/fail
+
+// let k = 0;
+
+// while (k > 5) {
+// console.log(k)
+// k++;
+// console.log ("Current val:" + k)
+// }
+
